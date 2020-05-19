cadddfad5dd0813d68eb5aad1b1b7b6d724dfbef
兰胜 <lansheng@hard.dhcp>
2020-05-19 22:08:34 +0800

    init: stress

---
+++ client/http.go
@@ -0,0 +1,62 @@
+package client
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/textproto"
+	"time"
+)
+
+func Get(url string, header http.Header) error {
+	rc, err := request("GET", url, header, bytes.NewBuffer(nil), time.Second*30)
+	if err != nil || rc == nil {
+		return err
+	}
+	defer func() { _ = rc.Close() }()
+	return nil
+}
+
+// Post 发起post请求
+func Post(url string, header http.Header, body, v interface{}) error {
+	data, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+	rc, err := request("POST", url, header, bytes.NewBuffer(data), 0)
+	if err != nil || rc == nil {
+		return err
+	}
+	defer func() { _ = rc.Close() }()
+	return nil
+}
+
+func request(method, url string, header http.Header, body io.Reader, timeout time.Duration) (io.ReadCloser, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   timeout,
+	}
+	// new a request
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	// set headers
+	for k, v := range header {
+		req.Header[textproto.CanonicalMIMEHeaderKey(k)] = v
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Body, nil
+}
---
+++ main.go
@@ -0,0 +1,65 @@
+/*
+golang开发的压力测试工具, 以goroutine模拟连接数
+
+stress -c 200 -n 10 -u http://0.0.0.0:8080/none
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+	"stress/client"
+	"sync"
+	"time"
+)
+
+var (
+	coroutines int
+	num        int
+	help       bool
+	url        string
+)
+
+func init() {
+	flag.StringVar(&url, "u", "", "链接")
+	flag.BoolVar(&help, "h", false, "帮助")
+	flag.IntVar(&num, "n", 0, "连接数")
+	flag.IntVar(&coroutines, "c", 0, "并发数")
+
+}
+
+func main() {
+	runtime.GOMAXPROCS(1)
+
+	var wg sync.WaitGroup
+
+	startTime := uint64(time.Now().UnixNano())
+	flag.Parse()
+	if help {
+		flag.Usage()
+		return
+	}
+
+	for i := 0; i < coroutines; i++ {
+		wg.Add(1)
+		go stress(url, &wg)
+	}
+
+	wg.Wait()
+	fmt.Printf("%.3f秒", float64(uint64(time.Now().UnixNano())-startTime)/1e9)
+}
+
+// stress 压力测试
+func stress(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < num; i++ {
+		time.Sleep(time.Millisecond * 1)
+		err := client.Get(url, http.Header{})
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
